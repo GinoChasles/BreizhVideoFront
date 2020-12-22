f3f452e29e773d8942df9f6c67b1a9c7ef7881e5
ginochasles <ginochasles@gmail.com>
2020-12-22 11:07:29 +0100

    tableau dynamique village

--- mon-app/src/App.js
+++ mon-app/src/App.js
@@ -1,5 +1,8 @@
 
 import './App.css';
+import Film from "./components/Film"
+import Village from "./components/Village"
+
 
 function App() {
   return (
@@ -9,8 +12,35 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
       </header>
+    
+    <Village data={data} apiUrl={"http://localhost:8080/breizvideo/villages"} />
     </div>
   );
 }
 
 export default App;
+
+const data = [
+  {name:'180nantes', post_code:'44300'},
+  {name:'180nantes', post_code:'44300'},
+  {name:'180nantes', post_code:'44300'},
+  {name:'180nantes', post_code:'44300'},
+  {name:'180nantes', post_code:'44300'}
+]
+fetch('http://localhost:8080/breizhvideo/actor/1', {
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': 'http://localhost:3000',
+                'Access-Control-Allow-Credentials': 'true'
+            },
+            mode: 'no-cors',
+            method: 'GET',
+            body: JSON.stringify()
+        })
+            .then((response) => {
+                return response.json();
+            })
+            .then((datas) => {
+                console.log(datas);
+            })
+            .catch(error => console.log(error));
---
+++ mon-app/src/components/Village.js
@@ -0,0 +1,44 @@
+import React, {useState,useEffect} from 'react'
+
+
+export default function Village({data, headerInFooter,apiUrl}) {
+    const keys = []
+
+    for(let k in data[0]){
+        keys.push(k)
+    }
+
+
+    const header = keys.map((key, index) => <th key={index}>{key}</th>)
+
+
+
+    return (
+        <>
+        <table>
+            <thead>
+              <tr>{header}</tr>
+            </thead>
+                 <tbody>
+                {
+                data.map((row, i) => (
+                    <tr key={i} data-index={i} >
+                       {
+                            keys.map((key, index) => <th key={index}
+                            >{row[key]}</th>)
+                        }
+                        <th><a href={apiUrl + `/${i}/update`}>update</a>
+                        </th>
+                        <th><a href={apiUrl + `/${i}/delete`}>delete</a>
+                        </th>
+                    </tr>
+                ))
+                }
+            </tbody>
+            <tfoot>
+                {headerInFooter ? <tr>{header}</tr> : null}
+            </tfoot>
+        </table>
+        </>
+    )
+}
