b9866c090c8ff9e9b1a3da8f8a682d65548bb01d
ginochasles <ginochasles@gmail.com>
2020-12-22 14:55:43 +0100

    customisation composant films

--- mon-app/src/components/Film.js
+++ mon-app/src/components/Film.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import {React, useContext} from 'react'
 import Accordion from 'react-bootstrap/Accordion'
+import AccordionContext from "react-bootstrap/AccordionContext";
 import Card from 'react-bootstrap/Card'
-import {Button} from 'react-bootstrap'
+import {Button, useAccordionToggle} from 'react-bootstrap'
 
 const data=[{id:1, TITLE:"academy dinosaur",DESCRIPTION:"	A Epic Drama of a Feminist And a Mad Scientist who must Battle a Teacher in The Canadian Rockies" ,RELEASE_YEAR:2006,LENGTH:86},
 {id: 2,TITLE:"ace goldfinger",DESCRIPTION:"A Astounding Epistle of a Database Administrator And a Explorer who must Find a Car in Ancient China" ,RELEASE_YEAR:2006,LENGTH:48},
@@ -10,21 +11,41 @@ const data=[{id:1, TITLE:"academy dinosaur",DESCRIPTION:"	A Epic Drama of a Femi
 export default function Film() {
     return (
         <Accordion>
-            {data.map((data,i)=>
-        <Card>
+            {data.map((data)=>
+        <Card key={data.id}>
             <Card.Header>
-                <Accordion.Toggle as={Button} variant="link" eventKey={data.i}>
                     {data.TITLE}
-                </Accordion.Toggle>
+                <Perso eventKey={data.id} />
+                
             </Card.Header>
-        <Accordion.Collapse eventKey={data.i}>
+        <Accordion.Collapse eventKey={data.id}>
             <Card.Body>
-                <p>année de sortie:{data.RELEASE_YEAR}</p>
-                <p>durée : {data.length}</p>
-                <p>description: {data.DESCRIPTION}</p>
+                <p>année de sortie : {data.RELEASE_YEAR}</p>
+                <p>durée : {data.LENGTH} min</p>
+                <p>{data.DESCRIPTION}</p>
             </Card.Body>
         </Accordion.Collapse>
         </Card>)}
 </Accordion>
     )
 }
+function Perso({ children, eventKey, callback }) {
+    const currentEventKey = useContext(AccordionContext);
+  
+    const decoratedOnClick = useAccordionToggle(
+      eventKey,
+      () => callback && callback(eventKey),
+    );
+  
+    const isCurrentEventKey = currentEventKey === eventKey;
+  
+    return (
+      <button
+        type="button"
+        style={{ backgroundColor: isCurrentEventKey ? 'pink' : 'lavender' }}
+        onClick={decoratedOnClick}
+      >
+        {children}
+      </button>
+    );
+  }
